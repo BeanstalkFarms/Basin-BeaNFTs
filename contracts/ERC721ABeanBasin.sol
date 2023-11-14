6143559c0313445e88ab73d5853c8d598ffa7bcd
nickkatsios <t8200071@aueb.gr>
2023-11-14 14:47:06 +0200

    comment changes and mapping verbosity

--- contracts/ERC721ABeanBasin.sol
+++ contracts/ERC721ABeanBasin.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Code adopted and modified from:
-// Chiru labs: ERC721AUpsgradable v4.2.3
+// Chiru labs: ERC721AUpgradable v4.2.3
 
 pragma solidity ^0.8.20;
 
@@ -8,26 +8,18 @@ import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import 'erc721a-upgradeable/contracts/ERC721AUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 
-/*
-* ERC721A Bean Basin
-* Contracts for the Basin NFT collection
-* Minted to users who deposited >1000 bdv in the 600 seasons 
-* post basin deployment
-* NFTs are batch minted to addresses on initialization
-* No need for a mint function
-*/
 contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
 
     // Mapping to mark token upgrades after 1800 seasons of deposit holding
-    // after basin deployment
-    mapping(uint256 => bool) public tokenUpgraded;
+    // after basin deployment.
+    mapping(uint256 id => bool upgraded) public isUpgraded;
 
     /*
-    * @param name_ string name of the NFT
-    * @param symbol_ string symbol of the NFT
-    * @param addresses_ array of addresses to mint to
-    * @param amt_ array of amounts to mint to each address
-    * Upon initialization, the contract will mint the NFTs to the addresses provided
+    * @param name_ string name of the NFT.
+    * @param symbol_ string symbol of the NFT.
+    * @param addresses_ array of addresses to mint to.
+    * @param amt_ array of amounts to mint to each address.
+    * Upon initialization, the contract will mint the NFTs to the addresses provided.
     */
     function initialize(string memory name_, string memory symbol_,address[] calldata addresses_, uint256[] calldata amounts_) initializerERC721A initializer public {
         __ERC721A_init(name_, symbol_);
@@ -36,27 +28,27 @@ contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgrade
     }
 
     /*
-    * Perform an upgrade of an ERC1967Proxy, when this contract
+    * Perform an upgrade of an ERC1967Proxy, when this contract.
     * is set as the implementation behind such a proxy.
-    * The _authorizeUpgrade function must be overridden
+    * The _authorizeUpgrade function must be overridden.
     * to include access restriction to the upgrade mechanism.
     */
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
     /*
-    * @dev __batchMintAllInit function used to mint NFT(s) to addresses
-    * @param addresses_ array of addresses to mint to
-    * @param amt_ array of amounts to mint to each address
-    * This function can only be called on initialization of the contract
+    * @dev __batchMintAllInit function used to mint NFT(s) to addresses.
+    * @param addresses_ array of addresses to mint to.
+    * @param amt_ array of amounts to mint to each address.
+    * This function can only be called on initialization of the contract.
     * 
     * _mintERC2309 mints `quantity` tokens and transfers them to `to`.
     *
-    * It emits only one {ConsecutiveTransfer} as defined in
+    * It emits only one {ConsecutiveTransfer} as defined in.
     * [ERC2309](https://eips.ethereum.org/EIPS/eip-2309),
     * instead of a sequence of {Transfer} event(s).
     */  
     function __batchMintAllInit(address[] calldata addresses, uint256[] calldata amount) internal onlyInitializingERC721A{
-        // check that length of addresses == length of amount 
+        // check that length of addresses == length of amount.
         require(addresses.length == amount.length, "ERC721ABeanBasin: length of addresses != length of amounts to mint");
         
         for(uint256 i; i < addresses.length; ++i){
@@ -65,8 +57,8 @@ contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgrade
     }
 
     /*
-    * @dev baseURI function used to return the IPFS baseURI for the metadata
-    * @return string baseURI of the NFT
+    * @dev baseURI function used to return the IPFS baseURI for the metadata.
+    * @return string baseURI of the NFT.
     */
     function baseURI() public view returns (string memory) {
         return _baseURI();
@@ -77,8 +69,8 @@ contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgrade
     }
 
     /*
-    * @dev baseURI function used to return the IPFS baseURI for the metadata
-    * @return string baseURI of the NFT
+    * @dev baseURI function used to return the IPFS baseURI for the metadata.
+    * @return string baseURI of the NFT.
     */
     function upgradedBaseURI() public view returns (string memory) {
         return _upgradedBaseURI();
@@ -95,8 +87,8 @@ contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgrade
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
 
-        if (tokenUpgraded[tokenId]) {
-            // return upgraded tokenURI with easter egg
+        if (isUpgraded[tokenId]) {
+            // return upgraded tokenURI with easter egg.
             string memory __upgradedBaseURI = _upgradedBaseURI();
             return bytes(__upgradedBaseURI).length != 0 ? string(abi.encodePacked(__upgradedBaseURI, _toString(tokenId),".json")) : '';
         } else {
@@ -106,17 +98,17 @@ contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgrade
     }
 
     /*
-    * @dev exists function used to check if a tokenId exists
-    * @param tokenId uint256 tokenId of NFT to check
-    * @return bool true if exists, false if not
+    * @dev exists function used to check if a tokenId exists.
+    * @param tokenId uint256 tokenId of NFT to check.
+    * @return bool true if exists, false if not.
     */
     function exists(uint256 tokenId) public view returns (bool) {
         return _exists(tokenId);
     }
 
     /*
-    * @dev burn function used to burn NFT(s)
-    * @param tokenId uint256 tokenId of NFT to burn
+    * @dev burn function used to burn NFT(s).
+    * @param tokenId uint256 tokenId of NFT to burn.
     */
     function burn(uint256 tokenId) public {
         _burn(tokenId);
@@ -137,15 +129,15 @@ contract ERC721ABeanBasin is ERC721AUpgradeable, OwnableUpgradeable, UUPSUpgrade
     }
 
     /*
-    * @dev upgradeNFTs function used to mark NFTs as upgraded
-    * Upgraded nfts with easter egg will have a different tokenURI
-    * Farmers will get an upgrade if they held their 
-    * BEANETH Deposit for 1000+ seasons
-    * @param tokenIds array of tokenIds to mark as upgraded
+    * @dev upgradeNFTs function used to mark NFTs as upgraded.
+    * Upgraded nfts with easter egg will have a different tokenURI.
+    * Farmers will get an upgrade if they held their.
+    * BEANETH Deposit for 1000+ seasons.
+    * @param tokenIds array of tokenIds to mark as upgraded.
     */
     function upgradeNFTs(uint256[] calldata tokenIds) public onlyOwner {
         for(uint256 i; i < tokenIds.length; ++i){
-            tokenUpgraded[tokenIds[i]] = true;
+            isUpgraded[tokenIds[i]] = true;
         }
     }
 
