b3d0f41d50add49a245180485171103fd5f8e679
nickkatsios <t8200071@aueb.gr>
2023-11-18 18:44:43 +0200

    Nft upgrade operations

--- scripts/upgradeNfts.js
+++ scripts/upgradeNfts.js
@@ -8,7 +8,8 @@ async function main(){
     const erc721ABeanBasin = await ethers.getContractAt("ERC721ABeanBasin", PROXY);
     console.log("Upgrading BeaNFT Basin NFTs");
     // provide an upgrade list based on token IDs to upgrade
-    await erc721ABeanBasin.upgradeNFTs([0, 1]);
+    const upgradeList = [1, 2, 4, 5, 6, 7, 8, 13, 14, 15, 16, 17, 20, 21, 23, 24, 25, 26, 27, 28, 32, 33, 34, 35, 36, 38, 39, 41, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 57, 58, 61, 62, 63, 65, 66, 67, 68, 69, 70, 71];
+    await erc721ABeanBasin.upgradeNFTs(upgradeList);
     const tokenURI = await erc721ABeanBasin.tokenURI(1);
     console.log(tokenURI);
     console.log("BeaNFT Basin NFTs Upgraded");
